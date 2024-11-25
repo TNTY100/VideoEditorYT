52b97b697071873201c3d184e7591027ca6cfbdf
Yohan Thibault <y2thibaultgm@gmail.com>
2024-11-25 14:08:20 -0500

    Ajout l'application javaFx

--- src/main/java/cal/a24/Main.java
+++ src/main/java/cal/a24/Main.java
@@ -1,7 +1,9 @@
 package cal.a24;
 
+import cal.a24.frontend.AppliMontage;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AppliMontage.main(args);
     }
 }
---
+++ src/main/java/cal/a24/frontend/AppliMontage.java
@@ -0,0 +1,18 @@
+package cal.a24.frontend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppliMontage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle("Sample Application");
+        stage.show();
+    }
+}
